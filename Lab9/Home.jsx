aa29358e63d278966bd24ba456daf033f6e9720f
Shreya Takoor <shreyatakoor12@gmail.com>
2026-04-07 11:59:12 +0530

    Add Home component with welcome message and image

---
+++ Lab9/Home.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+function Home() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '40px' }}>
+      <h2>Home Page</h2>
+
+      <img
+  src="https://images.unsplash.com/photo-1507525428034-b723cf961d3e"
+  alt="Home"
+  style={{ width: '400px', borderRadius: '10px', marginTop: '20px' }}
+/>
+
+      <p style={{ marginTop: '20px', fontSize: '18px' }}>
+        Welcome to our website! Explore our pages to learn more.
+      </p>
+    </div>
+  );
+}
+
+export default Home;
